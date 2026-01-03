92a037214bd5154e8f23e843ba008dbac6844ea4
SoumyaCode76 <soumyadeep.work02@gmail.com>
2026-01-03 11:39:35 +0000

    Completed code with testing

--- Sorting_programs/merge_sort.c
+++ Sorting_programs/merge_sort.c
@@ -3,6 +3,7 @@
 int X[] = { 12, -5, 7, -20, 3, -1, 18, -9, 0, 4 };
 
 void merge_sort_array(int * X, unsigned int low, unsigned int high);
+void sort_part_arrays(int * X, unsigned low, unsigned high);
 
 int main(void)
 {
@@ -25,5 +26,32 @@ int main(void)
 
 void merge_sort_array(int * X, unsigned int low, unsigned int high)
 {
-    return;
+    if(low >= high)
+    {
+        return;
+    }
+    else
+    {
+        unsigned int mid = (unsigned int)((low + high) / 2);
+        merge_sort_array(X, low, mid);
+        merge_sort_array(X, mid + 1, high);
+        /* When both the arrays are out, sort using selection sort algorithm */
+        sort_part_arrays(X, low, high);
+    }
+}
+
+void sort_part_arrays(int * X, unsigned low, unsigned high)
+{
+    for(int i = low; i <= high; i++)
+    {
+        for(int j = low; j < high - i; j++)
+        {
+            if(X[j] > X[j + 1])
+            {
+                X[j] = X[j] ^ X[j + 1];
+                X[j + 1] = X[j] ^ X[j + 1];
+                X[j] = X[j] ^ X[j + 1];
+            }
+        }
+    }
 }
