3ee2d06a76934827ee0dd79c18a97ccf4b06fb3b
SoumyaCode76 <soumyadeep.work02@gmail.com>
2026-02-09 13:06:02 +0000

    Interview prep content

---
+++ Array_problems/memory_copy.h
@@ -0,0 +1,8 @@
+#ifndef __MEMCPY_H__
+#define __MEMCPY_H__
+
+#include <stdio.h>
+
+int memory_copy(void * dst, const void * src, unsigned int length);
+
+#endif
---
+++ Bitwise/count_set_bits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t swap_odd_even_bits(uint32_t num, uint8_t bits)
+{
+    uint32_t even_mask = 0, odd_mask = 0;
+    for(uint8_t i = 0; i < bits; i++)
+    {
+        
+    }
+}
+
+int main(void)
+{
+    
+}
